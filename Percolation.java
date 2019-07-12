c29e17914d685c18e4e9f0971ceeee18c66ec865
Jiaqi Ding <jiaqiding.ricky@gmail.com>
2019-07-11 23:47:20 -0600

    bug is hard to fix

--- Percolation.java
+++ Percolation.java
@@ -4,21 +4,21 @@ public class Percolation {
     private WeightedQuickUnionUF uf;
     private int objectNum;
     private int gridWidth;
-    private int[] openCount;
-    private int openSum=0;
+    private boolean[] openCount;
+    private int openSum = 0;
 
     // creates n-by-n grid, with all sites initially blocked java.lang.IllegalArgumentException if any argument to open(), isOpen(), or isFull() is outside its prescribed range
     public Percolation(int n) {
         if (n <= 0) {
             throw new IllegalArgumentException("grid size out of range!");
-        }else{
+        } else {
             this.gridWidth = n;
             this.objectNum = n * n;
-            openCount = new int[objectNum+2];
+            openCount = new boolean[objectNum + 2];
             uf = new WeightedQuickUnionUF(objectNum + 2);
             for (int i = 1; i < n + 1; i++) {
                 uf.union(0, i);
-                uf.union(objectNum + 1 , objectNum + 1 - i);
+                uf.union(objectNum + 1, objectNum + 1 - i);
             }
         }
     }
@@ -26,63 +26,73 @@ public class Percolation {
     // opens the site (row, col) if it is not open already
     // union
     public void open(int row, int col) {
-        if (row < 0 || row > gridWidth || col < 0 || col > gridWidth) {
+        if (row <= 0 || row > gridWidth || col <= 0 || col > gridWidth) {
             throw new IllegalArgumentException("index is not between 0 and grid width!");
-        }else{
-            int siteNum = (row-1)*gridWidth+col;
-            openCount[siteNum] = 1;
-            openSum++;
-            validateAndOpen(row,col-1,siteNum);
-            validateAndOpen(row,col+1,siteNum);
-            validateAndOpen(row+1,col,siteNum);
-            validateAndOpen(row-1,col,siteNum);
+        } else {
+            if (!isOpen(row, col)) {
+                int siteNum = (row - 1) * gridWidth + col;
+                openCount[siteNum] = true;
+                openSum++;
+                validateAndOpen(row, col - 1, siteNum);
+                validateAndOpen(row, col + 1, siteNum);
+                validateAndOpen(row + 1, col, siteNum);
+                validateAndOpen(row - 1, col, siteNum);
+            }
         }
     }
 
     // is the site (row, col) open?
-    public boolean isOpen(int row, int col){
+    public boolean isOpen(int row, int col) {
         if (row < 0 || row > gridWidth || col < 0 || col > gridWidth) {
             throw new IllegalArgumentException("index is not between 0 and grid width!");
-        }else {
-            return openCount[(row - 1) * gridWidth + col] > 0;
+        } else {
+            return openCount[(row - 1) * gridWidth + col];
         }
     }
 
     // is the site (row, col) full?
-    public boolean isFull(int row, int col){
+    public boolean isFull(int row, int col) {
         if (row < 0 || row > gridWidth || col < 0 || col > gridWidth) {
             throw new IllegalArgumentException("index is not between 0 and grid width!");
-        }else {
-            if(isOpen(row, col)){
-                return  uf.find((row - 1) * gridWidth + col) == objectNum + 1 || uf.find((row - 1) * gridWidth + col) == 0;
-            }else {
+        } else {
+            if (isOpen(row, col)) {
+                if (percolates()) {
+                    return uf.find((row - 1) * gridWidth + col) == 0 || uf.find((row - 1) * gridWidth + col) == objectNum + 1;
+                } else {
+                    return uf.find((row - 1) * gridWidth + col) == 0;
+                }
+            } else {
                 return false;
             }
         }
     }
 
     // returns the number of open sites
-    public int numberOfOpenSites(){
+    public int numberOfOpenSites() {
         return openSum;
     }
 
     // does the system percolate?
-    public boolean percolates(){
-        return (uf.find(0)==uf.find(objectNum+1));
-    }
-
-    // validate that p is a valid index
-    private void validateAndOpen(int row,int col, int origin) {
-        if(row > 0 && row <= gridWidth && col > 0 && col <= gridWidth){
-            int siteNum = (row-1)*gridWidth+col;
-            if(isOpen(row, col)){
-                uf.union(siteNum,origin);
-            }
-        }
+    public boolean percolates() {
+        return (uf.find(0) == uf.find(objectNum + 1));
     }
 
     // test client (optional)
-    public static void main(String[] args){
+    public static void main(String[] args) {
+        // yes i just do not want to write it
+    }
 
+    // validate that p is a valid index
+    private void validateAndOpen(int row, int col, int siteNum) {
+        if (row > 0 && row <= gridWidth && col > 0 && col <= gridWidth) {
+            int origin = (row - 1) * gridWidth + col;
+            if (isOpen(row, col)) {
+                if (uf.find(origin) == 0 || uf.find(origin) == objectNum + 1) {
+                    uf.union(siteNum, origin);
+                } else {
+                    uf.union(origin, siteNum);
+                }
+            }
+        }
     }
 }
--- PercolationStats.java
+++ PercolationStats.java
@@ -2,60 +2,62 @@ import edu.princeton.cs.algs4.StdOut;
 import edu.princeton.cs.algs4.StdRandom;
 import edu.princeton.cs.algs4.StdStats;
 
-import java.util.Random;
 
 public class PercolationStats {
-    private double[] successNum;
+    private static final double FACTOR = 1.96;
+    private final double[] successNum;
+
     // perform independent trials on an n-by-n grid
-    public PercolationStats(int n, int trials){
-        this.successNum = new double[trials];
-        for(int i=0;i<trials;i++){
-            Percolation perc = new Percolation(n);
-            StdOut.println("start trial:"+i);
-            while (!perc.percolates()){
-                int row = StdRandom.uniform(1,n+1);
-                int col = StdRandom.uniform(1,n+1);
-                if(!perc.isOpen(row,col)){
-                    perc.open(row,col);
-                    StdOut.println("open:"+row+","+col);
+    public PercolationStats(int n, int trials) {
+        if (n <= 0 || trials <= 0) {
+            throw new IllegalArgumentException("grid size out of range!");
+        } else {
+            this.successNum = new double[trials];
+            for (int i = 0; i < trials; i++) {
+                Percolation perc = new Percolation(n);
+                while (!perc.percolates()) {
+                    int row = StdRandom.uniform(1, n + 1);
+                    int col = StdRandom.uniform(1, n + 1);
+                    if (!perc.isOpen(row, col)) {
+                        perc.open(row, col);
+                    }
                 }
+                this.successNum[i] = (perc.numberOfOpenSites() / (double) (n * n));
             }
-            StdOut.println("success trial:"+i);
-            this.successNum[i] = (perc.numberOfOpenSites() / (double)(n * n) );
         }
     }
 
     // sample mean of percolation threshold
-    public double mean(){
+    public double mean() {
         return StdStats.mean(successNum);
     }
 
     // sample standard deviation of percolation threshold
-    public double stddev(){
+    public double stddev() {
         return StdStats.stddev(successNum);
     }
 
     // low endpoint of 95% confidence interval
-    public double confidenceLo(){
-        return mean() - 1.96 * stddev() / Math.sqrt(successNum.length);
+    public double confidenceLo() {
+        return StdStats.mean(successNum) - FACTOR * StdStats.stddev(successNum) / Math.sqrt(successNum.length);
     }
 
     // high endpoint of 95% confidence interval
-    public double confidenceHi(){
-        return mean() + 1.96 * stddev() / Math.sqrt(successNum.length);
+    public double confidenceHi() {
+        return StdStats.mean(successNum) + FACTOR * StdStats.stddev(successNum) / Math.sqrt(successNum.length);
     }
 
     // test client (see below)
-    public static void main(String[] args){
-        int n = 10 ;
+    public static void main(String[] args) {
+        int n = 10;
         int trials = 20000;
         if (args.length == 2) n = Integer.parseInt(args[0]);
-        if (args.length == 2)  trials = Integer.parseInt(args[1]);
-        PercolationStats perc = new PercolationStats(n,trials);
-        StdOut.println("mean:"+perc.mean());
-        StdOut.println("stddev:"+perc.stddev());
-        StdOut.println("hi:"+perc.confidenceHi());
-        StdOut.println("lo:"+perc.confidenceLo());
+        if (args.length == 2) trials = Integer.parseInt(args[1]);
+        PercolationStats perc = new PercolationStats(n, trials);
+        StdOut.println("mean:" + perc.mean());
+        StdOut.println("stddev:" + perc.stddev());
+        StdOut.println("hi:" + perc.confidenceHi());
+        StdOut.println("lo:" + perc.confidenceLo());
     }
 
 }
